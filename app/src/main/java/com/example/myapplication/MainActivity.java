3dc141a239e506f9cbb75fac880b7f1a08f20491
ADFAL <fallahadnane@gmail.com>
2024-03-27 15:29:10 +0000

    Test 3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,10 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,7 +45,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if (v.getId() == R.id.btnUsersActLoadUsers) {
             UsersAdapter adapter = new UsersAdapter(this, getUsers());
-
             lvUsersActUsers.setAdapter(adapter);
         } else if (v.getId() == R.id.btnUsersActQuit) {
             finish();
@@ -80,5 +86,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         return usersFullNames;
+    }
+
+
+    public class UsersAdapter extends BaseAdapter {
+        Context context;
+        LayoutInflater inflater;
+        ArrayList<User> users;
+
+        public UsersAdapter(Context context, ArrayList<User> users) {
+            this.users = users;
+            this.context = context;
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return users.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return users.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            User user = users.get(position);
+            convertView = inflater.inflate(R.layout.item_user, null);
+
+            TextView tvUsersItmFullName = convertView.findViewById(R.id.tvUsersItmFullName);
+            TextView tvUsersItmCity = convertView.findViewById(R.id.tvUsersItmCity);
+            ImageButton tvUsersItemDetails = convertView.findViewById(R.id.tvUsersItemDetails);
+
+            tvUsersItmFullName.setText(user.fullName());
+            tvUsersItmCity.setText(user.getCity());
+            tvUsersItemDetails.setOnClickListener(v->{
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("firstName",user.getFirstName());
+                intent.putExtra("lastName",user.getLastName());
+                intent.putExtra("gender",user.getGender());
+                intent.putExtra("city",user.getCity());
+                startActivity(intent);
+            });
+
+            return convertView;
+        }
     }
 }
