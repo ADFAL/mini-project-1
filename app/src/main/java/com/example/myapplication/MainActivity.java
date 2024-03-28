5ce8253036bb2efad673f117e2f1fba4afd92903
ADFAL <fallahadnane@gmail.com>
2024-03-28 00:41:11 +0000

    Test 7

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnUsersActLoadUsers;
     TextView tvUsersActQuit;
     ListView lvUsersActUsers;
+    private ProgressBar progressBar;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -42,14 +47,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 finish();
             }
         });
+
+
+        // create ProgressBar in java code :
+        ConstraintLayout constraintLayout = findViewById(R.id.constraint_layout);
+        progressBar = new ProgressBar(this);
+        progressBar.setId(View.generateViewId());
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        progressBar.setLayoutParams(params);
+        constraintLayout.addView(progressBar);
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(constraintLayout);
+        constraintSet.connect(progressBar.getId(), ConstraintSet.TOP, constraintLayout.getId(), ConstraintSet.TOP, 0);
+        constraintSet.connect(progressBar.getId(), ConstraintSet.BOTTOM, constraintLayout.getId(), ConstraintSet.BOTTOM, 0);
+        constraintSet.connect(progressBar.getId(), ConstraintSet.START, constraintLayout.getId(), ConstraintSet.START, 0);
+        constraintSet.connect(progressBar.getId(), ConstraintSet.END, constraintLayout.getId(), ConstraintSet.END, 0);
+        constraintSet.applyTo(constraintLayout);
+        progressBar.setVisibility(View.GONE);
     }
+
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btnUsersActLoadUsers) {
-            UsersAdapter adapter = new UsersAdapter(this,R.layout.item_user,getUsers());
-            adapter.notifyDataSetChanged();
-            lvUsersActUsers.setAdapter(adapter);
+            progressBar.setVisibility(View.VISIBLE);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    UsersAdapter adapter = new UsersAdapter(MainActivity.this, R.layout.item_user, getUsers());
+                    lvUsersActUsers.setAdapter(adapter);
+                    progressBar.setVisibility(View.GONE);
+                }
+            }, 2000);
         }
     }
 
